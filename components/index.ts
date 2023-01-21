a9fcb35db4bda5af0c056d264243a725fb519af8
Adrián coll <nege2002@gmail.com>
2023-01-21 14:39:08 +0100

    fix: export from interfaces in social section

--- components/index.ts
+++ components/index.ts
@@ -4,7 +4,6 @@ export * from './SocialMediaIcon'
 export * from './SocialMediaRow'
 
 export { SocialSection } from './SocialSection'
-export type { Link, ISocialSection } from './SocialSection'
 
 export { LinkBox } from './LinkBox'
 export { DarkModeToggle } from './DarkModeToggle'
